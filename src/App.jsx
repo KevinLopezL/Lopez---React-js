3b9c0ffb0d33ffb4b0d8969914f92185981cde8d
Kevin Lopez <kevinlopeznom@gmail.com>
2023-03-29 15:53:06 -0300

    React Js Github

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ProductCard from './componentes/ProductCard/ProductCard';
+import Navbar from './componentes/Navbar/Navbar';
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <ProductCard />
+      
+    </div>
+  );
+}
+
+export default App;
